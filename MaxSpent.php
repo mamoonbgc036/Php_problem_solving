798ebc1f8655ad94f608e8d74b3fd410194b34ef
mamoonbgc036 <mamoon036@gmail.com>
2026-04-11 20:32:54 +0600

    Max spent passed

--- MaxSpent.php
+++ MaxSpent.php
@@ -1,3 +1,16 @@
 <?php 
 
-echo "max";
+function getMoneySpent($keyboard, $drivers, $b){
+    $maxSpent = -1;
+    foreach($keyboard as $key){
+        foreach($drivers as $driver){
+            $combinedTotal = $key + $driver;
+            if($combinedTotal > $maxSpent && $combinedTotal <= $b){
+                $maxSpent = $combinedTotal;
+            }
+        }
+    }
+    return $maxSpent;
+}
+
+echo getMoneySpent([3, 1], [5, 2, 8], 10);
