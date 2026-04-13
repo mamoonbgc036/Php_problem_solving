3a08c76e6cb82212cb492a118373d907cd489452
mamoonbgc036 <mamoon036@gmail.com>
2026-04-13 23:55:27 +0600

    cat and mouse problem solved

--- CatAndMouse.php
+++ CatAndMouse.php
@@ -1,8 +1,16 @@
 <?php 
 
 function catAndMouse($x, $y, $z) {
+    $distA = abs($x - $z);
+    $distB = abs($y - $z);
 
-
+    if ($distA < $distB) {
+        return "Cat A";
+    } elseif ($distB < $distA) {
+        return "Cat B";
+    } else {
+        return "Mouse C";
+    }
 }
 
-catAndMouse(1,2,3);
+echo catAndMouse(1,3,2);
