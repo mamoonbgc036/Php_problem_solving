cdea8c4d2bea49788cbb1860c86f54e1aabfd592
mamoonbgc036 <mamoon036@gmail.com>
2026-04-14 17:20:20 +0600

    magic square

---
+++ MagicSquare.php
@@ -0,0 +1,10 @@
+<?php 
+
+function formingMagicSquare($s) {
+    // take a variable with cost and min_cost
+    // foreach loop on predefined magic
+    // check equality 
+    // if not equal abs differenc and add to cost
+    // if cost if less than min_cost then update the min_cost with cost
+
+}
