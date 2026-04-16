86a7280a9323652be106548aa28de1ddfd02650d
mamoonbgc036 <mamoon036@gmail.com>
2026-04-16 07:01:09 +0600

    done

--- PickingNumber.php
+++ PickingNumber.php
@@ -1,5 +1,13 @@
 <?php 
 
 function PickingNumber($a){
-    
+    $count = array_count_values($a);
+    $max = 0;
+
+    foreach ($count as $num => $freq) {
+        $next = isset($count[$num + 1]) ? $count[$num + 1] : 0;
+        $max = max($max, $freq + $next);
+    }
+
+    return $max;
 }
